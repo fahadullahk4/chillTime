0db848cdffb5c74f1ef100a400a38437b8d164b9
Fahad Ullah Khan <fahadullahk4@gmail.com>
2025-06-14 19:55:22 +0500

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
 import { useDebounce } from "react-use";
-// import {client} from "appwrite"
-import { updateSearchCount } from "./appwrite";
+import { updateSearchCount, getTrendingMovies } from "./appwrite";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -20,12 +19,17 @@ const App = () => {
 	const [searchBar, setSearchBar] = useState("");
 	const [errorMessage, setErrorMessage] = useState("");
 	const [movieList, setMovieList] = useState([]);
+	const [trendingMovies, setTrendingMovies] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
-	const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("")
+	const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
 
-	useDebounce(() => {
-		setDebouncedSearchTerm(searchBar);
-	}, 500, [searchBar]);
+	useDebounce(
+		() => {
+			setDebouncedSearchTerm(searchBar);
+		},
+		500,
+		[searchBar]
+	);
 
 	const fetchMovies = async (query = "") => {
 		setIsLoading(true);
@@ -33,8 +37,8 @@ const App = () => {
 
 		try {
 			const endpoint = query
-			? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
-			: `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+				? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+				: `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
 			const response = await fetch(endpoint, API_OPTIONS);
 			if (!response.ok) {
@@ -51,7 +55,10 @@ const App = () => {
 			}
 
 			setMovieList(data.results || []);
-			updateSearchCount();
+
+			if (query && data.results.length > 0) {
+				await updateSearchCount(query, data.results[0]);
+			}
 		} catch (error) {
 			console.error(`Error fetching movies: ${error}`);
 			setErrorMessage(`Error fetching movies, please try again later.`);
@@ -60,9 +67,23 @@ const App = () => {
 		}
 	};
 
+	const loadTrendingMovies = async () => {
+		try {
+			const movies = await getTrendingMovies();
+
+			setTrendingMovies(movies);
+		} catch (error) {
+			console.error(`Error fetching trending movies: ${error}`);
+		}
+	};
+
 	useEffect(() => {
 		fetchMovies(debouncedSearchTerm);
 	}, [debouncedSearchTerm]);
+
+	useEffect(() => {
+		loadTrendingMovies();
+	}, []);
 	return (
 		<main>
 			<div className="pattern" />
@@ -77,8 +98,22 @@ const App = () => {
 					<Search searchBar={searchBar} setSearchBar={setSearchBar} />
 				</header>
 
+				{trendingMovies.length > 0 && (
+					<section className="trending">
+						<h2>Trending Movies</h2>
+						<ul>
+							{trendingMovies.map((movie, index) => (
+								<li key={movie.$id}>
+									<p>{index + 1}</p>
+									<img src={movie.poster_url} alt={movie.title} />
+								</li>
+							))}
+						</ul>
+					</section>
+				)}
+
 				<section className="all-movies">
-					<h2 className="mt-[40px]">All Movies</h2>
+					<h2>All Movies</h2>
 
 					{isLoading ? (
 						<Spinner />
@@ -98,5 +133,4 @@ const App = () => {
 		</main>
 	);
 };
-
 export default App;
--- src/appwrite.js
+++ src/appwrite.js
@@ -1,7 +1,52 @@
+import { Client, Databases, ID, Query } from "appwrite";
+
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+const ENDPOINT_ID = import.meta.env.VITE_APPWRITE_ENDPOINT;
 
-export const updateSearchCount = async () => {
-	console.log(PROJECT_ID, DATABASE_ID, COLLECTION_ID);
+const client = new Client().setEndpoint(ENDPOINT_ID).setProject(PROJECT_ID);
+
+const database = new Databases(client);
+// const movies = database.collection(COLLECTION_ID);
+
+export const updateSearchCount = async (searchBar, movie) => {
+	try {
+		const response = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+			Query.equal("searchBar", searchBar),
+		]);
+
+		if (response.documents.length > 0) {
+			const documentId = response.documents[0];
+			await database.updateDocument(
+				DATABASE_ID,
+				COLLECTION_ID,
+				documentId.$id,
+				{
+					count: documentId.count + 1,
+				}
+			);
+		} else {
+			await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+				searchBar,
+				count: 1,
+				poster_url: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
+				movie_id: movie.id,
+			});
+		}
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export const getTrendingMovies = async () => {
+	try {
+		const response = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+			Query.limit(5),
+			Query.orderDesc("count"),
+		]);
+        return response.documents
+	} catch (error) {
+		console.error(error);
+	}
 };
